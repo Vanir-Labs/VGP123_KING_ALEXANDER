c72b208720f0aab510cc3aab069b854d6ce00f63
Vanir-Labs <vanirlabs@gmail.com>
2021-05-15 06:59:43 -0400

    No major changes

--- Assets/Scripts/Player/PlayerProjectile.cs
+++ Assets/Scripts/Player/PlayerProjectile.cs
@@ -9,12 +9,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerProjectile : MonoBehaviour
 {                   //Change all "BananaBullet" to customized projectile 
 //    public GameObject BananaBullet;
+    public GameObject bullet;
     public float speed = 10f;
     public Transform FirePoint;
     public GameObject bulletPrefab;
+    public Rigidbody2D rb;
 
     // Start is called before the first frame update
     void Start()
@@ -34,8 +37,22 @@ public class PlayerProjectile : MonoBehaviour
 //            instBananaBulletRB.AddForce(Vector3.forward * speed);
             //Destroy projectile after (default 2f = 2 sec) 
 //            Destroy(instBananaBullet, 2f);
-        }
+        GameObject ball = Instantiate(bullet, transform.position, transform.rotation);
 
+        }
+        {
+            //simply rotate left or right(write this code in your script that controls player
+/*
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                FirePoint.transform.Rotate(0, -90, 0);
+            }
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                FirePoint.transform.Rotate(0, 90, 0);
+            }
+*/
+        }
         //Shooting action           Adjust Mouse0 to custom key
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
@@ -47,6 +64,12 @@ public class PlayerProjectile : MonoBehaviour
     //Shooting projectile
     void Shoot()
     {
-        Instantiate(bulletPrefab, FirePoint.position, FirePoint.rotation);
+//        Instantiate(bulletPrefab, FirePoint.position, FirePoint.rotation);
+
+        var Bullet = (GameObject)Instantiate(bulletPrefab, FirePoint.position, FirePoint.rotation);
+        // Add velocity to the bullet
+        Bullet.GetComponent<Rigidbody>().velocity = Bullet.transform.forward * 6;
+        // Destroy the bullet after 2 seconds
+        Destroy(Bullet, 2.0f);
     }
 }
